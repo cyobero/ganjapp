29d24d3bbfb11c550cc3b63d4005055c647b26e1
czar <cyobero@gmail.com>
2020-04-17 18:16:14 -0700

    added new admin models for products

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from products.models import Flower
+from products.models import Flower, PreRoll, Cartridge, Edible
+
+class CartridgeAdmin(admin.ModelAdmin):
+    pass
+
+class EdibleAdmin(admin.ModelAdmin):
+    pass
 
 class FlowerAdmin(admin.ModelAdmin):
     pass
 
+class PreRollAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Flower, FlowerAdmin)
+admin.site.register(Cartridge, CartridgeAdmin)
+admin.site.register(Edible, EdibleAdmin)
+admin.site.register(PreRoll, PreRollAdmin)
--- products/models.py
+++ products/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 
 FLOWER_KIND_CHOICES = (
-    ('S', "Sativa"),
+    ('H', 'Hybrid')
     ('I', 'Indica'),
-    ('H', 'Hybrid'),
+    ('S', 'Sativa')
+)
+
+EDIBLE_KIND_CHOICES = (
+    ('COOK', 'Cookie'),
+    ('GUM', 'Gummy')
+    ('CHOC', 'Chocolate'),
+    ('O', 'Other'),
 )
 
 # Create your models here.
@@ -17,7 +24,36 @@ class Flower(models.Model):
     cbd = models.DecimalField(max_digits=2, decimal_places=2, blank=True,
                               null=True)
     description = models.CharField(max_length=300, blank=True, null=True)
+    quantity = models.DecimalField(max_digits=3, decimal_places=1, blank=True,
+                                   null=True, default=0.0)
 
     class Meta:
         verbose_name = ("Flower")
         verbose_name_plural = ("Flowers")
+
+
+class PreRoll(models.Model):
+    flower = models.OneToOneField(Flower, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ("Pre-Roll")
+        verbose_name = ("Pre-Rolls")
+
+
+class Cartridge(models.Model):
+    flower = models.OneToOneField(Flower, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ("Cartridge")
+        verbose_name = ("Cartridges")
+
+
+class Edible(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    thc = models.DecimalField(max_digits=2, decimal_places=2, blank=True,
+                              null=True, default=0.0)
+
+
+    class Meta:
+        verbose_name = ("Edible")
+        verbose_name_plural = ("Edibles")
