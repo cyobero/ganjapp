12c6db3cf6ae6b386ca349fefb2901b1ffd0f85a
czar <cyobero@gmail.com>
2020-04-17 17:13:59 -0700

    create products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Flower(models.Model):
+    FLOWER_KIND_CHOICES = (
+        ("S", "Sativa"),
+        ("I", "Indica"),
+        ("H", "Hybrid"),
+    )
+    name = models.CharField(max_length=100)
+    kind = models.CharField(max_length=1, choices=FLOWER_KIND_CHOICES)
+    thc = models.DecimalField(max_digits=4, decimal_places=2,
+                              blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("Flower")
+        verbose_name_plural = _("Flowers")
