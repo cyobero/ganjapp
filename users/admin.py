5f005e7ad0fa20c99f229ce95ad7ebcd7495a856
czar <cyobero@gmail.com>
2020-04-17 18:58:49 -0700

    added username field to Profile model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from users.models import Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ('first_name', 'last_name', 'birth_date', 'profile_type', )
+
+admin.site.register(Profile, ProfileAdmin)
