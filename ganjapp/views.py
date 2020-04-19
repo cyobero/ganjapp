f032b79f1769dc63035ac44b9a7a491831068c49
czar <cyobero@gmail.com>
2020-04-19 07:51:47 -0700

    redirect user to signup page if user not logged in

--- ganjapp/views.py
+++ ganjapp/views.py
@@ -1,11 +1,14 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 from django.shortcuts import render, redirect, reverse
 from users.models import Profile
-from django.contrib.auth.decorators import login_required
 
 def index(request):
-    # redirect to login page if user is not
-    # logged in.
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        user = request.user
+        login(request, user)
+        return render(request, 'index.html')
+    return redirect(reverse('login'))
 
 
 def contact(request):
